7d8296684798493034222b298ee0dfa9e3ad70ca
Vaanyok <yv4n760@gmail.com>
2024-03-18 13:24:01 +0100

    changement on PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const app = express();
-const port = 3000; // Choisissez un port approprié
+const port = process.env.PORT || 3000; // Choisissez un port approprié
 app.use(cors());
 
 app.use(express.json());
